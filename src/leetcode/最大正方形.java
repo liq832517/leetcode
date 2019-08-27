1c403656707f73f32db9382cbd2d10bdb7bde1d4
lovoid <1004668310@qq.com>
2019-08-27 21:59:47 +0800

    221.最大正方形

---
+++ src/leetcode/最大正方形.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+public class 最大正方形 {
+    // dp[i][j] = min(dp[i-1][j],dp[i][j-1],dp[i-1][j-1])+1
+    public int maximalSquare(char[][] matrix) {
+
+        if (matrix.length == 0 || matrix[0].length == 0) return 0;
+        int[][] dp = new int[matrix.length + 1][matrix[0].length + 1];
+        int max = 0;
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                if (matrix[i - 1][j - 1] == '1') {
+                    dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                    max = Math.max(max, dp[i][j]);
+                }
+            }
+        }
+        return max * max;
+    }
+}
