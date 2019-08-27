9d2454e246c57a56135bd7cfbb97b15ed3e64367
lovoid <1004668310@qq.com>
2019-08-27 11:18:42 +0800

    9.回文数

---
+++ src/leetcode/回文数.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+public class 回文数 {
+    public static boolean solution(int x){
+
+        if(x<0 ||(x!=0&&x%10==0)) return false;
+        int ans = 0;
+        while (x>ans){
+            int pop = x%10;
+            ans = ans*10 + pop;
+            x/=10;
+        }
+        return ans == x ||ans/10 ==x;//考虑奇数位和偶数位
+    }
+}
