d72ffd164d675079f1281dde04b2b977a5d83967
lovoid <1004668310@qq.com>
2019-09-02 23:44:28 +0800

    269.反转链表

---
+++ src/leetcode/反转链表.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+public class 反转链表 {
+    public ListNode reverseList(ListNode head) {
+        ListNode cur = head;
+        ListNode pre = null;
+        while(cur!=null){
+            ListNode temp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = temp;
+        }
+        return pre;
+    }
+}
