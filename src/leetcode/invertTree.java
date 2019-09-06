eb94b741ed92af6d50958d34ec34c668f4ee2f32
lovoid <1004668310@qq.com>
2019-09-06 21:20:39 +0800

    226.翻转二叉树

---
+++ src/leetcode/invertTree.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+public class invertTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root!=null) {
+            TreeNode temp = root.left;
+            root.left = root.right;
+            root.right = temp;
+            invertTree(root.left);
+            invertTree(root.right);
+        }
+        return root;
+    }
+}
