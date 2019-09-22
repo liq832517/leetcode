9d24adba9767c885bdd5e92fc9f6c6d91cc32193
lovoid <1004668310@qq.com>
2019-09-22 18:40:34 +0800

    103. 二叉树的锯齿形层次遍历

---
+++ src/leetcode/zigzagLevelOrder.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+import java.util.*;
+
+public class zigzagLevelOrder {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+
+        List<List<Integer>> res =new ArrayList<>();
+        if(root==null) return res;
+        Queue<TreeNode> queue=new ArrayDeque<>();
+        queue.add(root);
+        TreeNode cur;
+        boolean f=true;
+        while (!queue.isEmpty()){
+            int t=queue.size();
+            List<Integer> temp=new ArrayList<>();
+            for (int i = 0; i < t; i++) {
+                cur=queue.poll();
+                temp.add(cur.val);
+                if(cur.left!=null)
+                    queue.add(cur.left);
+                if (cur.right!=null)
+                    queue.add(cur.right);
+            }
+            if(!f) Collections.reverse(temp);
+            f=!f;
+            res.add(temp);
+        }
+        return res;
+    }
+}
