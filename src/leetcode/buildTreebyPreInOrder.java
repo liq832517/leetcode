9e855396ba6c292065eb6111fd20a1a414b9fdae
lovoid <1004668310@qq.com>
2019-09-23 00:42:43 +0800

    105. 从前序与中序遍历序列构造二叉树

---
+++ src/leetcode/buildTreebyPreInOrder.java
@@ -0,0 +1,36 @@
+package leetcode;
+
+public class buildTreebyPreInOrder {
+
+    public TreeNode buildTreebyPreInOrder(int[] preorder, int[] inorder){
+        if (preorder.length == 0 || inorder.length == 0) return null;
+        int len = preorder.length;
+        return partition(preorder, inorder, 0, len - 1, 0);
+    }
+
+    private TreeNode partition(int[] preorder, int[] inorder, int inleft, int inright, int preleft) {
+        TreeNode root = new TreeNode(preorder[preleft]);
+        if(inleft == inright) return root;
+        int x = -1;
+
+        for (int i = inleft; i <= inright; i++) {
+            if (preorder[preleft] == inorder[i]) {
+                x = i;
+                break;
+            }
+        }
+
+        if (inleft == x || x == -1) {
+            root.left = null;
+        } else {
+            root.left = partition(preorder, inorder, inleft, x - 1, preleft + 1);
+        }
+        if (inright == x || x == -1) {
+            root.right = null;
+        } else {
+            root.right = partition(preorder, inorder, x + 1, inright, preleft + x - inleft + 1);
+        }
+
+        return root;
+    }
+}
--- src/leetcode/inorderTraversal.java
+++ src/leetcode/inorderTraversal.java
@@ -6,24 +6,23 @@ import java.util.Stack;
 
 public class inorderTraversal {
 
-    class Solution {
-        public List<Integer> inorderTraversal(TreeNode root) {
-            List<Integer> res = new ArrayList<Integer>();
-            if (root == null) {
-                return res;
-            }
-            Stack<TreeNode> stack = new Stack<TreeNode>();
-            TreeNode cur = root;
-            while (!stack.isEmpty()||cur!=null) {
-                while (cur != null) {
-                    stack.push(cur);
-                    cur = cur.left;
-                }
-                cur = stack.pop();
-                res.add(cur.val);
-                cur = cur.right;
-            }
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        if (root == null) {
             return res;
         }
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode cur = root;
+        while (!stack.isEmpty() || cur != null) {
+            while (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            res.add(cur.val);
+            cur = cur.right;
+        }
+        return res;
     }
+
 }
